560efcce85714fca6bd26a54e17831f61b26d26c
SummonHIM <2239760702@qq.com>
2022-10-17 18:50:32 +0800

    fix hw2t3

--- hw2/t3.c
+++ hw2/t3.c
@@ -5,10 +5,10 @@ int main()
     int a = 6, a1, a2, a3, a4;
     ++a;
     printf("前缀自加结果：%d \n", a);
-    a++;
+    a = 6; a++;
     printf("后缀自加结果：%d \n", a);
-    --a;
+    a = 6; --a;
     printf("前缀自减结果：%d \n", a);
-    a--;
+    a = 6; a--;
     printf("后缀自减结果：%d \n", a);
 }
